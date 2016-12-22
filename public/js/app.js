226fb031caf3d856d30e844662a2fa88d53795d1
Jeffrey Way <jeffrey@jeffrey-way.com>
2016-12-22 13:32:53 -0500

    Remove unused things

--- public/js/app.js
+++ public/js/app.js
@@ -1,154 +1,154 @@
 class Errors {
-	/**
-	 * Create a new Errors instance.
-	 */
-	constructor() {
-		this.errors = {};
-	}
+    /**
+     * Create a new Errors instance.
+     */
+    constructor() {
+        this.errors = {};
+    }
 
 
-	/**
-	 * Determine if an errors exists for the given field.
-	 */
-	has(field) {
-		return this.errors.hasOwnProperty(field);
-	}
+    /**
+     * Determine if an errors exists for the given field.
+     */
+    has(field) {
+        return this.errors.hasOwnProperty(field);
+    }
 
 
-	/**
-	 * Determine if we have any errors.
-	 */
-	any() {
-		return Object.keys(this.errors).length > 0;
-	}
+    /**
+     * Determine if we have any errors.
+     */
+    any() {
+        return Object.keys(this.errors).length > 0;
+    }
 
 
-	/**
-	 * Retrieve the error message for a field.
-	 */
-	get(field) {
-		if (this.errors[field]) {
-			return this.errors[field][0];
-		}
-	}
+    /**
+     * Retrieve the error message for a field.
+     */
+    get(field) {
+        if (this.errors[field]) {
+            return this.errors[field][0];
+        }
+    }
 
 
-	/**
-	 * Record the new errors.
-	 *
-	 * @param {object} errors
-	 */
-	record(errors) {
-		this.errors = errors;
-	}
+    /**
+     * Record the new errors.
+     *
+     * @param {object} errors
+     */
+    record(errors) {
+        this.errors = errors;
+    }
 
 
-	/**
-	 * Clear one or all error fields.
-	 *
-	 * @param {string|null} field
-	 */
-	clear(field) {
-		if (field) {
-			delete this.errors[field];
+    /**
+     * Clear one or all error fields.
+     *
+     * @param {string|null} field
+     */
+    clear(field) {
+        if (field) {
+            delete this.errors[field];
 
-			return;
-		}
+            return;
+        }
 
-		this.errors = {};
-	}
+        this.errors = {};
+    }
 }
 
 
 class Form {
-	/**
-	 * Create a new Form instance.
-	 * 
-	 * @param {object} data
-	 */
-	constructor(data) {
-		this.originalData = data;
+    /**
+     * Create a new Form instance.
+     * 
+     * @param {object} data
+     */
+    constructor(data) {
+        this.originalData = data;
 
-		for (let field in data) {
-			this[field] = data[field];
-		}
+        for (let field in data) {
+            this[field] = data[field];
+        }
 
-		this.errors = new Errors();
-	}
-
-
-	/**
-	 * Fetch all relevant data for the form.
-	 */
-	data() {
-		let data = Object.assign({}, this);
-
-		delete data.originalData;
-		delete data.errors;
-
-		return data;
-	}
+        this.errors = new Errors();
+    }
 
 
-	/**
-	 * Reset the form fields.
-	 */
-	reset() {
-		for (let field in this.originalData) {
-			this[field] = '';
-		}
-	}
+    /**
+     * Fetch all relevant data for the form.
+     */
+    data() {
+        let data = Object.assign({}, this);
+
+        delete data.originalData;
+        delete data.errors;
+
+        return data;
+    }
 
 
-	/**
-	 * Submit the form.
-	 * 
-	 * @param  {string} requestType 
-	 * @param  {string} url         
-	 */
-	submit(requestType, url) {
-		axios[requestType](url, this.data())
-			.then(this.onSuccess.bind(this))
-			.catch(this.onFail.bind(this))
-	}
+    /**
+     * Reset the form fields.
+     */
+    reset() {
+        for (let field in this.originalData) {
+            this[field] = '';
+        }
+    }
 
 
-	/**
-	 * Handle a successful form submission.
-	 * 
-	 * @param {object} response
-	 */
-	onSuccess(response) {
-		alert(response.data.message); // temporary
+    /**
+     * Submit the form.
+     * 
+     * @param  {string} requestType 
+     * @param  {string} url         
+     */
+    submit(requestType, url) {
+        axios[requestType](url, this.data())
+            .then(this.onSuccess.bind(this))
+            .catch(this.onFail.bind(this))
+    }
 
-		this.errors.clear();
-		this.reset();
-	}
 
-	/**
-	 * Handle a failed form submission.
-	 * 
-	 * @param {object} error
-	 */
-	onFail(error) {
-		this.errors.record(error.response.data);
-	}
+    /**
+     * Handle a successful form submission.
+     * 
+     * @param {object} response
+     */
+    onSuccess(response) {
+        alert(response.data.message); // temporary
+
+        this.errors.clear();
+        this.reset();
+    }
+
+    /**
+     * Handle a failed form submission.
+     * 
+     * @param {object} error
+     */
+    onFail(error) {
+        this.errors.record(error.response.data);
+    }
 }
 
 
 new Vue({
-	el: '#app',
+    el: '#app',
 
-	data: {
-		form: new Form({
-			name: '',
-			description: ''
-		})
-	},
+    data: {
+        form: new Form({
+            name: '',
+            description: ''
+        })
+    },
 
-	methods: {
-		onSubmit() {
-			this.form.submit('post', '/projects');
-		}
-	}
+    methods: {
+        onSubmit() {
+            this.form.submit('post', '/projects');
+        }
+    }
 });
--- resources/views/projects/create.blade.php
+++ resources/views/projects/create.blade.php
@@ -1,41 +1,41 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-	<meta charset="UTF-8">
-	<title>Document</title>
-	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bulma/0.2.3/css/bulma.css">
-	<style>body { padding-top: 40px; }</style>
+    <meta charset="UTF-8">
+    <title>Document</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bulma/0.2.3/css/bulma.css">
+    <style>body { padding-top: 40px; }</style>
 </head>
 
 <body>
-	<div id="app" class="container">
-		@include ('projects.list')		
+    <div id="app" class="container">
+        @include ('projects.list')      
 
-		<form method="POST" action="/projects" @submit.prevent="onSubmit" @keydown="form.errors.clear($event.target.name)">
-			<div class="control">
-				<label for="name" class="label">Project Name:</label>
-				
-				<input type="text" id="name" name="name" class="input" v-model="form.name"> 
+        <form method="POST" action="/projects" @submit.prevent="onSubmit" @keydown="form.errors.clear($event.target.name)">
+            <div class="control">
+                <label for="name" class="label">Project Name:</label>
+                
+                <input type="text" id="name" name="name" class="input" v-model="form.name"> 
 
-				<span class="help is-danger" v-if="form.errors.has('name')" v-text="form.errors.get('name')"></span>
-			</div>
+                <span class="help is-danger" v-if="form.errors.has('name')" v-text="form.errors.get('name')"></span>
+            </div>
 
-			<div class="control">
-				<label for="description" class="label">Project Description:</label>
-				
-				<input type="text" id="description" name="description" class="input" v-model="form.description">
+            <div class="control">
+                <label for="description" class="label">Project Description:</label>
+                
+                <input type="text" id="description" name="description" class="input" v-model="form.description">
 
-				<span class="help is-danger" v-if="form.errors.has('description')" v-text="form.errors.get('description')"></span>
-			</div>
+                <span class="help is-danger" v-if="form.errors.has('description')" v-text="form.errors.get('description')"></span>
+            </div>
 
-			<div class="control">
-				<button class="button is-primary" :disabled="form.errors.any()">Create</button>
-			</div>
-		</form>
-	</div>
+            <div class="control">
+                <button class="button is-primary" :disabled="form.errors.any()">Create</button>
+            </div>
+        </form>
+    </div>
 
-	<script src="https://cdnjs.cloudflare.com/ajax/libs/axios/0.15.3/axios.js"></script>
-	<script src="https://unpkg.com/vue@2.1.6/dist/vue.js"></script>
-	<script src="/js/app.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/axios/0.15.3/axios.js"></script>
+    <script src="https://unpkg.com/vue@2.1.6/dist/vue.js"></script>
+    <script src="/js/app.js"></script>
 </body>
 </html>
