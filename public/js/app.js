353edfe1de0190e0354eb3456a9c53fb0a215b4b
Jeffrey Way <jeffrey@jeffrey-way.com>
2016-12-22 13:26:39 -0500

    Part 2

--- public/js/app.js
+++ public/js/app.js
@@ -1,53 +1,154 @@
 class Errors {
+	/**
+	 * Create a new Errors instance.
+	 */
 	constructor() {
 		this.errors = {};
 	}
 
+
+	/**
+	 * Determine if an errors exists for the given field.
+	 */
 	has(field) {
 		return this.errors.hasOwnProperty(field);
 	}
 
+
+	/**
+	 * Determine if we have any errors.
+	 */
 	any() {
 		return Object.keys(this.errors).length > 0;
 	}
 
+
+	/**
+	 * Retrieve the error message for a field.
+	 */
 	get(field) {
 		if (this.errors[field]) {
 			return this.errors[field][0];
 		}
 	}
 
+
+	/**
+	 * Record the new errors.
+	 *
+	 * @param {object} errors
+	 */
 	record(errors) {
 		this.errors = errors;
 	}
 
+
+	/**
+	 * Clear one or all error fields.
+	 *
+	 * @param {string|null} field
+	 */
 	clear(field) {
-		delete this.errors[field];
+		if (field) {
+			delete this.errors[field];
+
+			return;
+		}
+
+		this.errors = {};
 	}
 }
+
+
+class Form {
+	/**
+	 * Create a new Form instance.
+	 * 
+	 * @param {object} data
+	 */
+	constructor(data) {
+		this.originalData = data;
+
+		for (let field in data) {
+			this[field] = data[field];
+		}
+
+		this.errors = new Errors();
+	}
+
+
+	/**
+	 * Fetch all relevant data for the form.
+	 */
+	data() {
+		let data = Object.assign({}, this);
+
+		delete data.originalData;
+		delete data.errors;
+
+		return data;
+	}
+
+
+	/**
+	 * Reset the form fields.
+	 */
+	reset() {
+		for (let field in this.originalData) {
+			this[field] = '';
+		}
+	}
+
+
+	/**
+	 * Submit the form.
+	 * 
+	 * @param  {string} requestType 
+	 * @param  {string} url         
+	 */
+	submit(requestType, url) {
+		axios[requestType](url, this.data())
+			.then(this.onSuccess.bind(this))
+			.catch(this.onFail.bind(this))
+	}
+
+
+	/**
+	 * Handle a successful form submission.
+	 * 
+	 * @param {object} response
+	 */
+	onSuccess(response) {
+		alert(response.data.message); // temporary
+
+		this.errors.clear();
+		this.reset();
+	}
+
+	/**
+	 * Handle a failed form submission.
+	 * 
+	 * @param {object} error
+	 */
+	onFail(error) {
+		this.errors.record(error.response.data);
+	}
+}
+
 
 new Vue({
 	el: '#app',
 
 	data: {
-		name: '',
-		description: '',
-		errors: new Errors()
+		form: new Form({
+			name: '',
+			description: ''
+		})
 	},
 
 	methods: {
 		onSubmit() {
-			axios.post('/projects', this.$data)
-				.then(this.onSuccess)
-				.catch(error => this.errors.record(error.response.data));
-		},
-
-		onSuccess(response) {
-			alert(response.data.message);
-
-			form.reset(); // Cliffhanger duh duh duh
+			this.form.submit('post', '/projects');
 		}
 	}
 });
-
-
--- resources/views/projects/create.blade.php
+++ resources/views/projects/create.blade.php
@@ -11,25 +11,25 @@
 	<div id="app" class="container">
 		@include ('projects.list')		
 
-		<form method="POST" action="/projects" @submit.prevent="onSubmit" @keydown="errors.clear($event.target.name)">
+		<form method="POST" action="/projects" @submit.prevent="onSubmit" @keydown="form.errors.clear($event.target.name)">
 			<div class="control">
 				<label for="name" class="label">Project Name:</label>
 				
-				<input type="text" id="name" name="name" class="input" v-model="name"> 
+				<input type="text" id="name" name="name" class="input" v-model="form.name"> 
 
-				<span class="help is-danger" v-if="errors.has('name')" v-text="errors.get('name')"></span>
+				<span class="help is-danger" v-if="form.errors.has('name')" v-text="form.errors.get('name')"></span>
 			</div>
 
 			<div class="control">
 				<label for="description" class="label">Project Description:</label>
 				
-				<input type="text" id="description" name="description" class="input" v-model="description">
+				<input type="text" id="description" name="description" class="input" v-model="form.description">
 
-				<span class="help is-danger" v-if="errors.has('description')" v-text="errors.get('description')"></span>
+				<span class="help is-danger" v-if="form.errors.has('description')" v-text="form.errors.get('description')"></span>
 			</div>
 
 			<div class="control">
-				<button class="button is-primary" :disabled="errors.any()">Create</button>
+				<button class="button is-primary" :disabled="form.errors.any()">Create</button>
 			</div>
 		</form>
 	</div>
